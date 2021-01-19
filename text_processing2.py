dfbec207d78c7a646055656c5f9168f87284bc06
yujin <yujin1760@naver.com>
2021-01-19 20:58:35 +0900

    Update my code

--- text_processing2.py
+++ text_processing2.py
@@ -71,7 +71,7 @@ def to_camel_case(underscore_str):
         underscore_str = underscore_str[:-1]
     tmp_str = underscore_str.split('_')
     if len(tmp_str) <= 1:
-        return tmp_str[0]
+        return ''.join(tmp_str)
     else:
         camelcase_str = ''.join(s.title() for s in tmp_str) 
         camelcase_str = camelcase_str[0].lower() + camelcase_str[1:]
